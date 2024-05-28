ec3ae69fa6efb48a9e04827c71ddb4eea70b397c
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2024-05-28 22:50:26 +0800

    can get relevent info for embed  :D

--- send_discord_embeds.py
+++ send_discord_embeds.py
@@ -1,7 +1,8 @@
 from ossapi import Ossapi, Score
-from data_to_html import get_data_at_date
+from data_to_html import get_data_at_date, compare_player_data, map_player_data
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
-import os, requests, math
+import argparse, os, requests, math
 
 def embed_maker(
     title:str=None,
@@ -72,7 +73,7 @@ def create_embed_from_play(data:Score):
 
     return embed_data
 
-def send_embed(content:str, embeds:list):
+def send_embed(content:str=':)', embeds:list=[]):
     webhook_url = os.getenv('WEBHOOK_URL')
 
     payload = {
@@ -90,24 +91,93 @@ def send_embed(content:str, embeds:list):
     else:
         print(f'Failed to send embed. Status code: {response.status_code}')
 
-def main():
+def sort_data_dictionary(data:dict, key:str, reversed:bool=False) -> dict:
+    return dict(
+        # what the fuck is this, python
+        sorted(data.items(), key=lambda x: x[1][key], reverse=reversed)
+    )
+
+def get_sorted_list_on_stat(data:dict, stat:str, highest_first:bool=False) -> dict:
+    # this is so goofy as fuck
+    return {
+        i: data[i]
+            for i in sort_data_dictionary(data, stat, highest_first)
+                if data[i][stat] != 0
+    }
+
+def main(country:str='PH', mode:str='fruits', test:bool=False):
     client_id = os.getenv('OSU_CLIENT_ID')
     client_secret = os.getenv('OSU_CLIENT_SECRET')
     api = Ossapi(client_id, client_secret)
     
-    users = [
-        # just for testing for now, me and top 4 ph
-        9025855, 829284, 16355636, 6829103, 1626093
+    latest_date = datetime.now()
+    comparison_date = latest_date - timedelta(days=1)
+    
+    # this should be in a function, since i feel like im going to repeat this a lot
+    # TODO: simplify this and do the above comment, thanks
+    latest_data = get_data_at_date(
+        latest_date.strftime('%Y/%m/%d'),
+        country=country,
+        mode=mode,
+        test=test,
+    )
+    
+    comparison_data = get_data_at_date(
+        comparison_date.strftime('%Y/%m/%d'),
+        country=country,
+        mode=mode,
+        test=test,
+    )
+    
+    data_difference = compare_player_data(
+        map_player_data(latest_data),
+        map_player_data(comparison_data)
+    )
+    
+    active_players = get_sorted_list_on_stat(data_difference, 'play_count', True)
+    pp_gainers = get_sorted_list_on_stat(data_difference, 'pp', True)
+    rank_gainers = get_sorted_list_on_stat(data_difference, 'rank', True)
+    
+    active_fields = [
+        {
+            'name': active_players[i]['ign'],
+            'value': str(active_players[i]['play_count']) + ' play count',
+        } for i in active_players
     ]
-
-    for u in users:
-        score = get_recent_top_play_of_user(api, u)
-        embed = create_embed_from_play(score)
-        send_embed(
-            content=f'Most recent top play',
-            embeds=[embed]
-        )
+    
+    pp_fields = [
+        {
+            'name': pp_gainers[i]['ign'],
+            'value': str(pp_gainers[i]['pp']) + 'pp',
+        } for i in pp_gainers
+    ]
+    
+    rank_fields = [
+        {
+            'name': rank_gainers[i]['ign'],
+            'value': str(rank_gainers[i]['rank']) + 'pp',
+        } for i in rank_gainers if rank_gainers[i]['rank'] > 0
+    ]
+    
+    send_embed('active and pp gainers', [
+        embed_maker(title='rank gains', fields=rank_fields[:10]),
+        embed_maker(title='play count uwu',fields=active_fields[:10]),
+        embed_maker(title='top pp gainers for today',fields=pp_fields[:10]),
+    ])
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate page from fetched data, requires leaderboard_scrape.py to be ran first!')
+    
+    parser.add_argument('--mode', type=str, default='fruits', help='Define what mode, uses the parameters used on osu site.')
+    parser.add_argument('--country', type=str, default='PH', help='What country to make a page from. Uses 2 letter country codes.')
+    parser.add_argument('--test', action='store_true', help='Just do tests')
+    
+    args = parser.parse_args()
+    
     load_dotenv()
-    main()
+    
+    main(
+        country=args.country,
+        mode=args.mode,
+        test=args.test,
+    )
