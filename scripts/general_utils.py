936542f3dd6327970ea0f96c93b9a36dab40dfe5
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2024-06-11 20:41:52 +0800

    add general utils for functions i feel i cannot group but will repeat, i think

---
+++ scripts/general_utils.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta, timezone
+
+def timestamp_utc_offset(
+    timestamp: float,
+    time_offset: int,
+    time_format: str,
+) -> str:
+    dt_object = datetime.fromtimestamp(timestamp, tz=timezone.utc)
+    dt_timezone = timezone(timedelta(hours=time_offset))
+    dt_timezone = dt_object.astimezone(dt_timezone)
+    formatted_time = dt_timezone.strftime(time_format)
+    
+    return formatted_time
