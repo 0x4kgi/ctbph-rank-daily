5843f78dd84183164fade59d1da83b84a9f57025
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2024-06-17 17:40:30 +0800

    initial implementation of a logger

---
+++ scripts/logging_config.py
@@ -0,0 +1,29 @@
+import logging
+
+def setup_logging(
+    level: int = logging.INFO,
+) -> logging.Logger:
+    # Assisited by ChatGPT(tm)
+
+    logger = logging.getLogger(name='ctbph-rank-daily')
+    logger.setLevel(level=level)
+
+    # Create handlers
+    # tbh i dont really know why
+    ch = logging.StreamHandler()
+    ch.setLevel(level=level)
+
+    # Create formatters and add them to handlers
+    # https://docs.python.org/3/library/logging.html#formatter-objects
+    # https://docs.python.org/3/library/logging.html#logrecord-attributes
+    formatter = logging.Formatter(
+        fmt='[{levelname:<7}] ({filename}:{lineno}:{funcName}) >> {message}',
+        style='{'
+    )
+    ch.setFormatter(fmt=formatter)
+
+    # Add handlers to the logger
+    logger.handlers = []  # Clear existing handlers
+    logger.addHandler(ch)
+
+    return logger
