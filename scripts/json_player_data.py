fd7a6ad1fbe4cabfa1b38acf9da174d72693207e
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2024-06-22 20:35:42 +0800

    satisfy the ide with the changes

--- scripts/json_player_data.py
+++ scripts/json_player_data.py
@@ -1,8 +1,11 @@
+import json
+import os
 from collections import namedtuple
 from datetime import datetime, timedelta
-from typing import Optional, TypedDict
+from typing import Optional, TypedDict, NotRequired
+
 from scripts.logging_config import logger
-import json, os
+
 
 class MappedPlayerData(TypedDict):
     """Object for player data
@@ -35,6 +38,9 @@ class MappedPlayerData(TypedDict):
     total_score: int
     ranked_score: int
     total_hits: int
+    new_entry: Optional[bool]
+    country_rank: Optional[int]
+
 
 class MappedScoreData(TypedDict):
     """Object for score data
@@ -63,16 +69,16 @@ class MappedScoreData(TypedDict):
     score_mods: str
     score_pp: float
     score_grade: str
-    
+
     user_id: int
     user_name: str
-    
+
     beatmapset_title: str
     beatmap_version: str
     beatmap_id: int
     beatmapset_id: int
     beatmap_difficulty: float
-    
+
     full_combo: bool
     max_combo: int
     count_300: int
@@ -80,6 +86,7 @@ class MappedScoreData(TypedDict):
     count_50: int
     count_miss: int
     accuracy: float
+
 
 class MappedPlayerDataCollection(TypedDict):
     """A collection of player data that includes the player id
@@ -101,51 +108,58 @@ class MappedPlayerDataCollection(TypedDict):
 class MappedScoreDataCollection(TypedDict):
     __getitem__: MappedScoreData
 
+
 class RawPlayerDataCollection(TypedDict):
-    """Object type when getting the contents of the json data file
+    """Object score_type when getting the contents of the json data file
     ```
     file_version: Optional[int]
+    file_type: Optional[str]
     update_date: float
     mode: str
     country: str
     pages: int
-    map: list[str]
+    key_mapping: list[str]
+    key: Optional[str]
     data: dict[str, list[int | str | float]]
     ```
     """
     file_version: Optional[float]
+    type: NotRequired[str]
     update_date: float
-    file_type: Optional[str]
+    file_type: NotRequired[str]
     mode: str
     country: str
-    pages: Optional[int]
+    pages: NotRequired[int]
     map: list[str]
+    key: NotRequired[str]
     data: dict[str, list[int | str | float]]
 
+
 def sort_data_dictionary(
-    data:MappedPlayerDataCollection,
-    key:str,
-    reversed:bool=False
+        data: MappedPlayerDataCollection,
+        key: str,
+        highest_first: bool = False
 ) -> MappedPlayerDataCollection:
     """Sorts mapped player data. 
 
     Args:
         data (dict[str,dict]): The mapped player data
         key (str): The field to sort from
-        reversed (bool, optional): Can be interpreted as "highest first?". Defaults to False.
+        highest_first (bool, optional): Can be interpreted as "highest first?". Defaults to False.
 
     Returns:
         dict: The mapped player data, now sorted to specified key.
     """
     logger.debug(f'sorting dict with {key=}')
     return dict(
-        sorted(data.items(), key=lambda x: x[1].get(key, 0), reverse=reversed)
+        sorted(data.items(), key=lambda x: x[1].get(key, 0), reverse=highest_first)
     )
 
+
 def get_sorted_dict_on_stat(
-    data:MappedPlayerDataCollection,
-    stat:str,
-    highest_first:bool=False
+        data: MappedPlayerDataCollection,
+        stat: str,
+        highest_first: bool = False
 ) -> MappedPlayerDataCollection:
     """Sorts mapped player data and removes 0 values.
 
@@ -164,7 +178,8 @@ def get_sorted_dict_on_stat(
         if data[i].get(stat, 0) != 0
     }
 
-def get_json(file_path: str, test: bool) -> dict:
+
+def get_json(file_path: str, test: bool) -> dict | None:
     """Gets the json file at the specified path. Can be specified if grabbing from tests
 
     Args:
@@ -175,63 +190,66 @@ def get_json(file_path: str, test: bool) -> dict:
         dict: The json data in dict
     """
     current_dir = os.path.dirname(__file__)
-        
+
     if test:
         file_path = 'tests/' + file_path
 
     logger.debug(f'trying to get {file_path}...')
-    
+
     file_path = os.path.join(current_dir, '../' + file_path)
-    
+
     try:
         with open(file_path) as file:
             data = json.load(file)
     except OSError as osx:
         print(f'{file_path} does not exist.')
         return None
-        
+
     return data
 
+
 def get_data_at_date(
-    date:str,
-    country:str,
-    mode:str,
-    file_type:str=None,
-    test:bool=False
-) -> RawPlayerDataCollection|None:
+        date: str,
+        country: str,
+        mode: str,
+        file_type: str = None,
+        test: bool = False
+) -> RawPlayerDataCollection | None:
     """Gets the json data file for the date, country, mode specified
 
     Args:
         date (str): The date, in YYYY/MM/DD format
         country (str): Uses 2 letter country code
         mode (str): osu/taiko/fruits/mania
+        file_type (str, optional): specifies the file score_type to get, this is just getting appended to the end
         test (bool, optional): Uses files in tests/ to avoid cluttering up main files. Defaults to False.
 
     Returns:
         dict: json as dictionary, `None` if the file does not exist yet
     """
-    
+
     # TODO: maybe do something about this
     if file_type is not None:
         target_file = f'data/{date}/{country}-{mode}-{file_type}.json'
     else:
         target_file = f'data/{date}/{country}-{mode}.json'
-    
+
     return get_json(file_path=target_file, test=test)
 
+
 def compare_player_data(
-    today_data:MappedPlayerDataCollection,
-    yesterday_data:MappedPlayerDataCollection
+        today_data: MappedPlayerDataCollection,
+        yesterday_data: MappedPlayerDataCollection
 ) -> MappedPlayerDataCollection:
     data = {}
-    
+
     for t in today_data:
         player = today_data.get(t)
         y_player = yesterday_data.get(t)
         data[t] = {
             'new_entry': False
         }
-        
+
         if y_player is None:
             data[t]['new_entry'] = True
             # temporary band-aid fix
@@ -241,36 +259,38 @@ def compare_player_data(
                     continue
                 data[t][stat] = 0
             continue
-        
+
         for stat in player:
             if stat == 'ign':
                 data[t][stat] = player[stat]
                 continue
-                
+
             t_stat = player.get(stat, 0)
             y_stat = y_player.get(stat, 0)
             difference = t_stat - y_stat
-            
+
             if stat == 'country_rank':
                 difference = 0 - difference
-            
+
             data[t][stat] = difference
 
     return data
 
-def map_player_data(data:RawPlayerDataCollection) -> MappedPlayerDataCollection|MappedScoreDataCollection:
-    decoded_data:MappedPlayerDataCollection = {}
-    
+
+def map_player_data(data: RawPlayerDataCollection) -> MappedPlayerDataCollection | MappedScoreDataCollection:
+    decoded_data: MappedPlayerDataCollection = {}
+
     map = data['map']
     player_data = data['data']
 
     for player in player_data:
         decoded_data[player] = {
-            map[i]: player_data[player][i] 
+            map[i]: player_data[player][i]
             for i in range(len(map))
         }
-    
+
     return decoded_data
+
 
 ComparisonAndMappedData = namedtuple(
     typename='ComparisonAndMappedData',
@@ -283,12 +303,13 @@ ComparisonAndMappedData = namedtuple(
     ]
 )
 
+
 def get_comparison_and_mapped_data(
-    base_date:datetime,
-    compare_date_offset:int,
-    country:str,
-    mode:str,
-    test:bool
+        base_date: datetime,
+        compare_date_offset: int,
+        country: str,
+        mode: str,
+        test: bool
 ) -> ComparisonAndMappedData:
     """Tries to compare the latest data from a specified date and older data 
     from the offset from the latest date and maps the data to become a valid
@@ -308,10 +329,10 @@ def get_comparison_and_mapped_data(
         tuple[3]: latest data timestamp
         tuple[4]: comparison data timestamp
     """
-    
+
     latest_date = base_date
     comparison_date = latest_date - timedelta(days=compare_date_offset)
-    
+
     latest_data_timestamp = None
     comparison_data_timestamp = None
 
@@ -334,7 +355,7 @@ def get_comparison_and_mapped_data(
         comparison_data_timestamp = comparison_data['update_date']
         comparison_mapped_data = map_player_data(comparison_data)
         data_difference = compare_player_data(latest_mapped_data, comparison_mapped_data)
-    
+
     return ComparisonAndMappedData(
         latest_mapped_data=latest_mapped_data,
         comparison_mapped_data=comparison_mapped_data,
